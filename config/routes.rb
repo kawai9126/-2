fc8869afd3849b4d79c42cfb3fb13aad3b0aa0c0
kawai9126 <iroha.9126@gmail.com>
2021-06-03 12:26:56 +0000

    12

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'home/about' => 'homes#about'
-   resources :books
+  resources :books
   resources :users,only: [:show,:index,:edit,:update]
 
 end
